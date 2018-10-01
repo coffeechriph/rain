8a252a20ab4e19f02bf3ec763b4ce6cccf173b8b
Christopher Sjöblom <coffeechriph@gmail.com>
2018-10-01 18:53:36 +0200

    INCOMPLETE: Began thinking about a more streamlined way of creating descriptor sets

--- src/rain/vulkan/DescriptorPool.kt
+++ src/rain/vulkan/DescriptorPool.kt
@@ -91,5 +91,110 @@ internal class DescriptorPool {
         return descriptorSetLayout
     }
 
+    class Builder {
+        private val logicalDevice: LogicalDevice
+        private val pool: DescriptorPool
+        private val uniformBuffers = ArrayList<UniformBufferDescriptor>()
+        private val textures = ArrayList<Texture2dDescriptor>()
+
+        constructor(logicalDevice: LogicalDevice, pool: DescriptorPool) {
+            this.logicalDevice = logicalDevice
+            this.pool = pool
+        }
+
+        fun withUniformBuffer(uniformBuffer: UniformBuffer, stageFlags: Int): Builder {
+            uniformBuffers.add(UniformBufferDescriptor(uniformBuffer, stageFlags))
+            return this
+        }
+
+        fun withTexture2d(texture2d: VulkanTexture2d, stageFlags: Int): Builder {
+            textures.add(Texture2dDescriptor(texture2d, stageFlags))
+            return this
+        }
+
+        fun build(): List<DescriptorSet> {
+            var bindingIndex = 0
+
+            val descriptorSets = ArrayList<DescriptorSet>()
+            for (u in uniformBuffers) {
+                val ubo = createUniformBufferSet(logicalDevice, u.stageFlags, null, u.buffer.buffer.size, u.buffer, bindingIndex)
+                bindingIndex += 1
+
+                val descriptorSet = DescriptorSet(ubo.descriptorSet, ubo.layout, ubo.uniformBuffer)
+                descriptorSets.add(descriptorSet)
+            }
+
+            for (t in textures) {
+
+            }
+
+            return descriptorSets
+        }
+
+        private fun createLayout(logicalDevice: LogicalDevice, stageFlags: Int, immutableSampler: LongBuffer?): LongBuffer {
+            // TODO: Change descriptorCount whenever we want to support more than 1 descriptor set / pipeline
+            val descriptorLayout = VkDescriptorSetLayoutBinding.calloc(1)
+                    .descriptorCount(1)
+                    .descriptorType(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER)
+                    .binding(0)
+                    .stageFlags(stageFlags)
+                    .pImmutableSamplers(immutableSampler)
+
+            val descriptorSetCreateInfo = VkDescriptorSetLayoutCreateInfo.calloc()
+                    .sType(VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO)
+                    .pBindings(descriptorLayout)
+
+            val descriptorSetLayout = memAllocLong(1)
+            val err = vkCreateDescriptorSetLayout(logicalDevice.device, descriptorSetCreateInfo, null, descriptorSetLayout)
+            if (err != VK_SUCCESS) {
+                throw AssertionError("Failed to create descriptor set layout " + VulkanResult(err))
+            }
+
+            return descriptorSetLayout
+        }
+
+        fun createUniformBufferSet(logicalDevice: LogicalDevice, stageFlags: Int, immutableSampler: LongBuffer?, descriptorCount: Int, uniformBuffer: UniformBuffer, bindingIndex: Int): DescriptorSet {
+            val layout = createLayout(logicalDevice, stageFlags, immutableSampler)
+
+            // We reuse the same layout, but are expecting an array of 1 layout per UBO
+            val pLayouts = memAllocLong(descriptorCount);
+            for (i in 0 until descriptorCount) {
+                pLayouts.put(i, layout[0]);
+            }
+
+            val descriptorSetInfo = VkDescriptorSetAllocateInfo.calloc()
+                    .sType(VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO)
+                    .descriptorPool(pool.pool)
+                    .pSetLayouts(pLayouts)
+
+            val descriptorSets = memAllocLong(descriptorCount)
+            val err = vkAllocateDescriptorSets(logicalDevice.device, descriptorSetInfo, descriptorSets)
+            if (err != VK_SUCCESS) {
+                throw AssertionError("Unable to create descriptor sets " + VulkanResult(err))
+            }
+
+            for (i in 0 until descriptorCount) {
+                val bufferInfo = VkDescriptorBufferInfo.calloc(1)
+                        .buffer(uniformBuffer.buffer[i])
+                        .offset(0)
+                        .range(VK_WHOLE_SIZE)
+
+                val descriptorWrite = VkWriteDescriptorSet.calloc(1)
+                        .sType(VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET)
+                        .dstSet(descriptorSets[i])
+                        .dstBinding(bindingIndex)
+                        .dstArrayElement(0)
+                        .descriptorType(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER)
+                        .pBufferInfo(bufferInfo)
+
+                vkUpdateDescriptorSets(logicalDevice.device, descriptorWrite, null)
+            }
+
+            return DescriptorSet(descriptorSets, layout[0], uniformBuffer)
+        }
+    }
+
+    private class UniformBufferDescriptor(val buffer: UniformBuffer, val stageFlags: Int)
+    private class Texture2dDescriptor(val texture: VulkanTexture2d, val stageFlags: Int)
     class DescriptorSet(val descriptorSet: LongBuffer, val layout: Long, val uniformBuffer: UniformBuffer)
 }
