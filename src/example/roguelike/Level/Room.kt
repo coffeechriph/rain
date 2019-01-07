49ea605aabbe86c101404e9ddd2ae1dfcb1f6e3b
Christopher Sjöblom <coffeechriph@gmail.com>
2019-01-07 21:13:02 +0100

    Move logic to generate room content to the room class

--- src/example/roguelike/Level/Room.kt
+++ src/example/roguelike/Level/Room.kt
@@ -1,11 +1,12 @@
 package example.roguelike.Level
 
-import example.roguelike.Entity.Container
-import example.roguelike.Entity.Enemy
-import example.roguelike.Entity.LightSource
-import org.joml.Random
-import org.joml.Vector2i
-import org.joml.Vector4i
+import com.badlogic.gdx.physics.box2d.BodyDef
+import example.roguelike.Entity.*
+import org.joml.*
+import rain.api.entity.DirectionType
+import rain.api.entity.EntitySystem
+import rain.api.gfx.Material
+import rain.api.gfx.ResourceFactory
 
 enum class RoomType {
     DIRT_CAVE,
@@ -62,5 +63,145 @@ class Room(val tiles: MutableList<Vector2i>, val area: Vector4i, val type: RoomT
         }
 
         return viableTiles.removeAt(rand.nextInt(viableTiles.size))
+    }
+
+    internal fun generateEnemiesInRoom(random: Random, enemySystem: EntitySystem<Enemy>, enemyMaterial: Material, player: Player, count: Int, healthBarSystem: EntitySystem<HealthBar>, healthBarMaterial: Material) {
+        for (i in 0 until count) {
+            val p = findNoneEdgeTile(random)
+            if (p == null) {
+                return
+            }
+
+            val enemy = random.nextInt(2)
+
+            val kracGuy = if (enemy == 0) {
+                Krac(random)
+            } else {
+                MiniKrac(random)
+            }
+            enemySystem.newEntity(kracGuy)
+                    .attachTransformComponent()
+                    .attachSpriteComponent(enemyMaterial)
+                    .attachAnimatorComponent()
+                    .attachBoxColliderComponent(width = 60.0f, height = 40.0f, aliveOnStart = false)
+                    .build()
+
+            val levelFactor = (player.currentLevel*1.5f).toInt()
+            kracGuy.strength = (random.nextInt(levelFactor) + levelFactor*5 * kracGuy.strengthFactor).toInt()
+            kracGuy.agility = (random.nextInt(levelFactor) + levelFactor*2 * kracGuy.agilityFactor).toInt()
+            kracGuy.health = (100 + random.nextInt(levelFactor) * kracGuy.healthFactor).toInt()
+            kracGuy.sprite.visible = false
+
+            val et = enemySystem.findTransformComponent(kracGuy.getId())!!
+            kracGuy.collider.setDamping(0.0f)
+            kracGuy.collider.setFriction(0.0f)
+            kracGuy.healthBar.parentTransform = et
+
+            healthBarSystem.newEntity(kracGuy.healthBar)
+                    .attachTransformComponent()
+                    .attachSpriteComponent(healthBarMaterial)
+                    .build()
+
+            kracGuy.healthBar.sprite.visible = false
+            kracGuy.healthBar.transform.sx = 60.0f
+            kracGuy.healthBar.transform.sy = 7.0f
+
+            kracGuy.setPosition(Vector2i(p.x*64, p.y*64))
+            enemies.add(kracGuy)
+        }
+    }
+
+    internal fun generateContainersInRoom(random: Random, count: Int, containerSystem: EntitySystem<Container>, itemMaterial: Material, resourceFactory: ResourceFactory) {
+        for (i in 0 until count) {
+            val tile = findNoneEdgeTile(random)
+            if (tile == null) {
+                return
+            }
+
+            val container = Container(random.nextInt(2), random.nextInt(7) + 1)
+            containerSystem.newEntity(container)
+                    .attachTransformComponent()
+                    .attachSpriteComponent(itemMaterial)
+                    .attachBoxColliderComponent(64.0f, 48.0f, BodyDef.BodyType.StaticBody)
+                    .attachBurstParticleEmitter(resourceFactory, 25, 16.0f, 0.2f, Vector2f(0.0f, -50.0f), DirectionType.LINEAR, 32.0f, 0.5f)
+                    .build()
+
+            val emitter = containerSystem.findBurstEmitterComponent (container.getId())!!
+            emitter.burstFinished = true
+            emitter.singleBurst = true
+            emitter.particlesPerBurst = 5
+            emitter.startColor = Vector4f(0.4f, 0.4f, 0.4f, 1.0f)
+            emitter.endColor = Vector4f(0.4f, 0.4f, 0.4f, 0.0f)
+            emitter.transform.z = 16.0f
+            emitter.enabled = false
+
+            container.setPosition(Vector2i(tile.x*64 + 32, tile.y*64 + 32))
+            container.collider.setDensity(1000.0f)
+            container.collider.setFriction(1.0f)
+            container.sprite.visible = false
+            container.collider.setActive(false)
+            containers.add(container)
+        }
+    }
+
+    internal fun generateLightsInRoom(random: Random, map: IntArray, mapWidth: Int, width: Int, height: Int, torches: Int, campfire: Boolean, torchSystem: EntitySystem<LightSource>, torchMaterial: Material, resourceFactory: ResourceFactory) {
+        var numTorches = 0
+        for (tile in tiles) {
+            val x = tile.x
+            val y = tile.y
+            if (y > 0 && map[x + (y-1)*mapWidth] == 1) {
+                val tx = x % width
+                val ty = y % height
+                val et = LightSource(x / width, y / height, Vector3f(0.9f, 0.55f, 0.1f))
+                torchSystem.newEntity(et)
+                        .attachTransformComponent()
+                        .attachSpriteComponent(torchMaterial)
+                        .attachParticleEmitter(resourceFactory, 10, 16.0f, 1.0f, Vector2f(0.0f, -10.0f), DirectionType.LINEAR, 4.0f, 0.5f)
+                        .build()
+                val etTransform = torchSystem.findTransformComponent(et.getId())
+                etTransform!!.setPosition((tx*64 + 32).toFloat(), (ty*64 - 32).toFloat(), 18.0f)
+                etTransform.sx = 48.0f
+                etTransform.sy = 48.0f
+
+                val sprite = torchSystem.findSpriteComponent(et.getId())!!
+                sprite.visible = false
+
+                val emitter = torchSystem.findEmitterComponent(et.getId())!!
+                emitter.startSize = 5.0f
+                emitter.startColor.set(1.0f, 0.9f, 0.2f, 1.0f)
+                emitter.endColor.set(1.0f, 0.3f, 0.0f, 0.5f)
+                emitter.enabled = false
+                this.torches.add(et)
+                numTorches++
+            }
+
+            if (numTorches >= torches) {
+                break
+            }
+        }
+
+        if (campfire) {
+            val tile = findNoneEdgeTile(random)
+            if (tile != null) {
+                val tx = tile.x % width
+                val ty = tile.y % height
+                val et = LightSource(tile.x / width, tile.y / height, Vector3f(0.9f, 0.55f, 0.1f))
+                torchSystem.newEntity(et)
+                        .attachTransformComponent()
+                        .attachParticleEmitter(resourceFactory, 20, 40.0f, 0.7f, Vector2f(0.0f, -50.0f), DirectionType.LINEAR, 20.0f, 0.5f)
+                        .build()
+                val etTransform = torchSystem.findTransformComponent(et.getId())
+                etTransform!!.setPosition(((tx*64) + 32).toFloat(), ((ty*64) - 32).toFloat(), 18.0f)
+                etTransform.sx = 64.0f
+                etTransform.sy = 64.0f
+
+                val emitter = torchSystem.findEmitterComponent(et.getId())!!
+                emitter.startSize = 20.0f
+                emitter.startColor.set(1.0f, 0.9f, 0.2f, 1.0f)
+                emitter.endColor.set(0.8f, 0.2f, 0.0f, 0.0f)
+                emitter.enabled = false
+                this.campfire.add(et)
+            }
+        }
     }
 }
